1920cbda13ea65e23018857c4672c307a3101e6c
phamduythanh12 <phamduythanh19998@gmail.com>
2019-10-29 21:25:07 +0700

    cart

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,31 +1,50 @@
 package model;
 
 public class Cart {
-	private Product product;
-	private int quatity;
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	public int getQuatity() {
-		return quatity;
-	}
-	public void setQuatity(int quatity) {
-		this.quatity = quatity;
-	}
-	public Cart() {
-		super();
-	}
-	public Cart(Product product, int quatity) {
-		super();
-		this.product = product;
-		this.quatity = quatity;
-	}
-	@Override
-	public String toString() {
-		return "Cart [product=" + product + ", quatity=" + quatity + "]";
-	}
-	
+
+    private int id;
+    private Product product;
+    private int quatity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuatity() {
+        return quatity;
+    }
+
+    public void setQuatity(int quatity) {
+        this.quatity = quatity;
+    }
+
+    public Cart() {
+        super();
+    }
+
+    public Cart(int id, Product product, int quatity) {
+        this.id = id;
+        this.product = product;
+        this.quatity = quatity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+   
+
+    @Override
+    public String toString() {
+        return "Cart [product=" + product + ", quatity=" + quatity + "]";
+    }
+
 }
